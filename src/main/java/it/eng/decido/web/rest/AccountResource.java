f2a3524012ae7eb2fc058c505038e42270696c85
Marco Martorana <74554879+marcomartorana@users.noreply.github.com>
2021-03-11 12:48:57 +0100

    minor

--- src/main/java/it/eng/decido/web/rest/AccountResource.java
+++ src/main/java/it/eng/decido/web/rest/AccountResource.java
@@ -46,7 +46,9 @@ public class AccountResource {
 
     private static class AccountResourceException extends RuntimeException {
 
-        private AccountResourceException(String message) {
+		private static final long serialVersionUID = 1L;
+
+		private AccountResourceException(String message) {
             super(message);
         }
     }
