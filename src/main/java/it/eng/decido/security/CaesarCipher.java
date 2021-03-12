e6061625cd20111346f68f5ed8e32affa077472d
Marco Martorana <74554879+marcomartorana@users.noreply.github.com>
2021-03-12 11:26:44 +0100

    Add CaesarCipher

--- src/main/java/it/eng/decido/security/CaesarCipher.java
+++ src/main/java/it/eng/decido/security/CaesarCipher.java
@@ -1,77 +1,78 @@
 package it.eng.decido.security;
 
 public class CaesarCipher {
-	private static String values = "-+%£$1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzòàèìù !?*\\	";
-	
-	/**
-	 * Encrypt string with CaesarCipher
-	 * 
-	 * @param text
-	 * @param key
-	 * @return String
-	 */
-	public static String encrypt(String text, int key) {
-		char charEnc;
-		int valEnc;
-		int newEnc;
-		String encrypted = "";
-		for (int i = 0; i < text.length(); i++) {
-			charEnc = text.charAt(i);
-			valEnc = values.indexOf(charEnc);
-			newEnc = (key + valEnc) % values.length();
-			encrypted += values.charAt(newEnc);
-		}
-		return encrypted;
-	}
+	private static final int DEFAULT_SHIFT = 3;
 
 	/**
-	 * Decrypt string with CaesarCipher
+	 * Encodes the modern Caesar Cipher by offsetting the characters by +3.
 	 * 
-	 * @param text
-	 * @param key
-	 * @return String
+	 * @param uncoded the word or words to encode
+	 * @return the encoded phrase
 	 */
-	public static String decrypt(String text, int key) {
-		char charEnc;
-		int valEnc;
-		int newEnc;
-		String decoded = "";
-		for (int i = 0; i < text.length(); i++) {
-			charEnc = text.charAt(i);
-			valEnc = values.indexOf(charEnc);
-			newEnc = (valEnc - key) % values.length();
-			// check for negative
-			if (newEnc < 0) {
-				newEnc += Math.abs(values.length());
-			}
-			decoded += values.charAt(newEnc);
-		}
-		return decoded;
+	public static String encode(String uncoded) {
+		return encode(uncoded, DEFAULT_SHIFT);
 	}
- 
-    public static void main(String[] args) {
-        String originalText = "Danny JHONSON	PeoPPle \\n\\l MIMMO ! ? !*** -+%£1234567890";
-        
-        int shiftCount = 1;
-        System.out.println("Caesar Cipher Example");
-        System.out.println("Encryption");
-        System.out.println("Text  : " + originalText);
-        System.out.println("Shift : " + shiftCount);
-        
-        String cipher = encrypt(originalText, shiftCount).toString();
-        System.out.println("Encrypted Cipher: " + cipher);
-        System.out.println("Decryption");
-        System.out.println("Encrypted Cipher: " + cipher);
-        System.out.println("Shift : " + shiftCount);
-        String decryptedPlainText = decrypt(cipher, shiftCount).toString();
-        System.out.println("Decrypted Plain Text  : " + decryptedPlainText);
-        
-        if(decryptedPlainText.equals(originalText)) {
-        	System.out.println("OK: originalText and decryptedPlainText are EQUALS !");
-        } else {
-        	System.out.println("KO: originalText and decryptedPlainText are NOT EQUALS !!!!");
-        }
-        
-    }
-    
+	
+	/**
+	 * Decodes the modern Caesar Cipher by offsetting the characters by -3.
+	 * 
+	 * @param encoded the word or words to decode
+	 * @return the decoded phrase
+	 */
+	public static String decode (String encoded) {
+		return decode(encoded, DEFAULT_SHIFT);
+	}
+	
+	/**
+	 * Encodes the modern Caesar Cipher by offsetting the characters by passed parameter.
+	 * 
+	 * @param uncoded the word or words to encode
+	 * @param offset shift
+	 * @return the encoded phrase
+	 */
+	public static String encode(String uncoded, int shift) {
+		char [] coder = uncoded.toCharArray();
+		for (int i = 0; i < coder.length; i++) {
+			coder[i] = (char)(coder[i] + shift);
+		}
+		return String.valueOf(coder);
+	}
+	
+	/**
+	 * Decodes the modern Caesar Cipher by offsetting the characters by passed parameter.
+	 * 
+	 * @param encoded the word or words to decode
+	 * @param offset shift
+	 * @return the decoded phrase
+	 */
+	public static String decode (String encoded, int shift) {
+		char [] coder = encoded.toCharArray();
+		for (int i = 0; i < coder.length; i++) {
+			coder[i] = (char)(coder[i] - shift);
+		}
+		return String.valueOf(coder);
+	}
+	
+	
+	public static void main(String[] args) {
+		System.out.println("---------------------------------");
+		System.out.println("--------  Caesar Cipher  --------");
+		System.out.println("---------------------------------");
+		String originalText = "Danny JHONSON	Mànèlidà PeoPPle \\\\n\\\\l MIMMO ! ? !*** -+%£1234567890";
+		System.out.println("originalText => "+originalText);
+		
+		String encripted = CaesarCipher.encode(originalText);
+		System.out.println("encripted => "+encripted);
+		
+		String decripted = CaesarCipher.decode(encripted);
+		System.out.println("decripted => "+decripted);
+		System.out.println("---------------------------------");
+		if (decripted.equals(originalText)) {
+			System.out.println("OK ===> decripted == originalText");
+		} else {
+			System.out.println("KO ===> decripted != originalText (Action should be provided!!)");
+		}
+		System.out.println("---------------------------------");
+	}
+	
 }
